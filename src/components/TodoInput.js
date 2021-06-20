372e9858399c597c2f58198b8dd3075d7e6208b1
Gopesh <medayilgopesh@gmail.com>
2021-06-20 10:40:56 +0530

    Implemented AddTodo functionality

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,11 +1,27 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addTodo } from "../redux/action";
+import { v4 as uuid } from "uuid";
 function TodoInput() {
+  const [name, setName] = useState("");
+  const dispatch = useDispatch();
   return (
     <React.Fragment>
       <div className="row m-2">
-        <input type="text" className="col form-control" />
-        <button className="btn btn-primary">ADD</button>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          className="col form-control"
+        />
+        <button
+          className="btn btn-primary"
+          onClick={() => dispatch(addTodo({ id: uuid(), name: name }))}
+        >
+          ADD
+        </button>
       </div>
     </React.Fragment>
   );
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -5,7 +5,9 @@ export let reducer = (state = todos, action) => {
   switch (action.type) {
     case ADD_TODO:
       console.log("In ADDTODO", action);
-      return state;
+      let newTodo = [...state];
+      newTodo.push(action.payload);
+      return newTodo;
       break;
     case UPDATE_TODO:
       break;
